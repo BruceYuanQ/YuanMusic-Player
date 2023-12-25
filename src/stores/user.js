e6c9d919a463abfadc836d66427696a6859b5484
bruce__yuan <2777374725@qq.com>
2023-12-25 16:59:37 +0800

    剩余页面实现，组件实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,16 +1,88 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { EL_CONFIG } from '@/config'
 
 // 用户模块
 export const useUserStore = defineStore(
-  'big-user',
+  'user',
   () => {
-    const token = ref('') // 定义 token
-    const setToken = (t) => (token.value = t) // 设置 token
+    const uid = ref('')
+    const historyList = ref([])
+    const volume = ref(EL_CONFIG.VOLUME)
+    const HISTORYLIST_MAX_LENGTH = 200
+    const avatarUrl = ref('')
+    const nickname = ref('')
+    // 设置登录用户uid
+    const setUid = (u) => {
+      uid.value = u
+    }
+    // 设置音量
+    const setVolume = (vol) => {
+      volume.value = vol
+    }
+    // 设置播放历史
+    const setHistoryList = (list) => {
+      historyList.value = list
+    }
 
-    return { token, setToken }
+    // 设置用户头像
+    const setAvatarUrl = (newUrl) => {
+      avatarUrl.value = newUrl
+    }
+
+    // 设置网名
+    const setNickname = (newName) => {
+      nickname.value = newName
+    }
+
+    // 添加音乐
+    const addHistoryMusic = (music) => {
+      const list = [...historyList.value]
+      const index = list.findIndex((item) => item.id === music.id)
+      if (index === 0) {
+        return
+      }
+      if (index > 0) {
+        list.splice(index, 1)
+      }
+      list.unshift(music)
+      if (list.length > HISTORYLIST_MAX_LENGTH) {
+        list.pop()
+      }
+      setHistoryList(list)
+    }
+
+    // 清空播放历史
+    const clearHistoryList = () => {
+      setHistoryList([])
+    }
+
+    // 删除播放历史
+    const deleteHistoryMusic = (index) => {
+      let list = historyList.value
+      list.splice(index, 1)
+      setHistoryList(list)
+    }
+
+    return {
+      uid,
+      setUid,
+      historyList,
+      setHistoryList,
+      addHistoryMusic,
+      clearHistoryList,
+      deleteHistoryMusic,
+      volume,
+      setVolume,
+      avatarUrl,
+      setAvatarUrl,
+      nickname,
+      setNickname
+    }
   },
   {
-    persist: true // 持久化
+    persist: {
+      paths: ['uid', 'historyList', 'volume', 'avatarUrl', 'nickname']
+    }
   }
 )
